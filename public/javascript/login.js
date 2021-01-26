8ec9051090a7d20718daf5c91a6cf7271bc524ae
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-26 07:35:01 -0600

    logout js file function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,8 @@
 async function loginFormHandler(e) {
     e.preventDefault();
 
-    const username = document.querySelector().value.trim();
-    const password = document.querySelector().value.trim();
+    const username = document.querySelector("#username-login").value.trim();
+    const password = document.querySelector("password-login").value.trim();
 
     if(username && password){
         const response = await fetch('/api/users/login', {
--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,8 +1,8 @@
 async function signupFormHandler(e){
     e.preventDefault();
 
-    const username = document.querySelector().value.trim();
-    const password = document.querySelector().value.trim();
+    const username = document.querySelector("#username-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
 
     if(username && password){
         const response = await fetch('/api/users', {
