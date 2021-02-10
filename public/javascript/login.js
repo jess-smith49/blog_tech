1d8614f62bdb6195f06f18eb4c4ef40422e228ae
jess-smith49 <js.jessicasmith774@gmail.com>
2021-02-10 12:04:25 -0600

    forgot to add id hashtag, LOGIN SIGNUP WORKING

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,7 +2,7 @@ async function loginFormHandler(e) {
     e.preventDefault();
 
     const username = document.querySelector("#username-login").value.trim();
-    const password = document.querySelector("password-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
 
     if(username && password){
         const response = await fetch('/api/users/login', {
